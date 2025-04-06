b470b4ba128b76c6c20a7034faf3b913bfa5662b
Ana Luiza <analuiza.daso@gmail.com>
2025-04-06 18:27:32 -0300

    Definir backgroud, instalar vuestify e axios.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
