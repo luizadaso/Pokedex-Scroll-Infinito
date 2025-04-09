fe4f1d814b4366ab4b435b632657af5c8bd91870
Ana Luiza <analuiza.daso@gmail.com>
2025-04-09 15:11:56 -0300

    Adcionar aba Evoluções na tabela com imagens e levels. Adicionar cores para os tipos e deixar em letras maiúsculas.

--- src/components/PokemonDialog.vue
+++ src/components/PokemonDialog.vue
@@ -1,186 +1,293 @@
 <template>
-    <v-dialog :value="showDialog" @input="$emit('update:showDialog', $event)" width="80%">
-      <v-card v-if="selectedPokemon" class="pokemon-dialog">
-        <v-row>
-          <v-col class="d-flex justify-end">
-            <v-btn icon @click="closeDialog" class="close-btn">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
+  <v-dialog :value="showDialog" @input="$emit('update:showDialog', $event)" width="80%">
+    <v-card v-if="selectedPokemon" class="pokemon-dialog">
+      <v-row>
+        <v-col class="d-flex justify-end">
+          <v-btn icon @click="closeDialog" class="close-btn">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-container>
+        <v-row class="d-flex align-center">
+          <v-col cols="8" class="text-center">
+            <h1>{{ getName(selectedPokemon) }}</h1>
+            <v-divider class="my-4"></v-divider>
+            <v-chip
+              class="ml-2"
+              v-for="type in selectedPokemon.types"
+              :key="type.slot"
+              :color="getTypeColor(type.type.name)"
+            >
+              {{ type.type.name.toUpperCase() }}
+            </v-chip>
+            <v-divider class="my-4"></v-divider>
+            <v-chip>Altura: {{ selectedPokemon.height * 2.54 }} cm</v-chip>
+            <v-chip class="ml-2">Peso: {{ (selectedPokemon.weight * 0.453592).toFixed(0) }} kg</v-chip>
+          </v-col>
+          <v-col cols="3">
+            <img
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`"
+              :alt="selectedPokemon.name" 
+              class="pokemon-image-front"
+            />
+            <img 
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${selectedPokemon.id}.png`"
+              :alt="selectedPokemon.name + ' back'" 
+              class="pokemon-image"
+            />
+            <img 
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${selectedPokemon.id}.png`"
+              :alt="selectedPokemon.name + ' shiny'" 
+              class="pokemon-image"
+            />
           </v-col>
         </v-row>
-        <v-container>
-          <v-row class="d-flex align-center">
-            <v-col cols="8" class="text-center">
-              <h1>{{ getName(selectedPokemon) }}</h1>
-              <v-chip class="ml-2" v-for="type in selectedPokemon.types" :key="type.slot">
-                {{ type.type.name }}
-              </v-chip>
-              <v-divider class="my-4"></v-divider>
-              <v-chip>Altura: {{ selectedPokemon.height * 2.54 }} cm</v-chip>
-              <v-chip class="ml-2">Peso: {{ (selectedPokemon.weight * 0.453592).toFixed(0) }} kg</v-chip>
-            </v-col>
-            <v-col cols="3">
-              <img
-                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`"
-                :alt="selectedPokemon.name" 
-                class="pokemon-image-front"
-              />
-              <img 
-                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${selectedPokemon.id}.png`"
-                :alt="selectedPokemon.name + ' back'" 
-                class="pokemon-image"
-              />
-              <img 
-                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${selectedPokemon.id}.png`"
-                :alt="selectedPokemon.name + ' shiny'" 
-                class="pokemon-image"
-              />
-            </v-col>
-          </v-row>
-          <v-tabs v-model="activeTab">
-            <v-tab>Movimentos</v-tab>
-            <v-tab>Versões de Jogos</v-tab>
-          </v-tabs>
-          <v-tabs-items v-model="activeTab">
-            <v-tab-item>
-              <v-data-table
-                :headers="moveHeaders"
-                :items="filterMoves(selectedPokemon)"
-                item-key="move.name"
-                show-expand
-                class="elevation-1"
-              >
-                <template v-slot:expanded-item="{ headers, item }">
-                  <td :colspan="headers.length">
-                    <v-list dense>
-                      <v-list-item v-for="version in item.version_group_details" :key="version.version_group.name">
-                        <v-list-item-content>
-                          <v-list-item-title>
-                            {{ version.version_group.name }}: Level {{ version.level_learned_at }}
-                          </v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </v-list>
-                  </td>
-                </template>
-              </v-data-table>
-            </v-tab-item>
-            <v-tab-item>
-              <v-data-table
-                :headers="gameHeaders"
-                :items="selectedPokemon.game_indices"
-                item-key="version.name"
-                class="elevation-1"
-              >
-                <template v-slot:item="{ item }">
-                  <tr>
-                    <td>{{ item.version.name }}</td>
-                  </tr>
-                </template>
-              </v-data-table>
-            </v-tab-item>
-          </v-tabs-items>
-        </v-container>
-      </v-card>
-    </v-dialog>
-  </template>
-  
-  <script>
-  export default {
-    name: 'PokemonDialog',
-    props: {
-      showDialog: {
-        type: Boolean,
-        required: true,
-      },
-      selectedPokemon: {
-        type: Object,
-        required: false,
-      },
+        <v-tabs v-model="activeTab">
+          <v-tab>Movimentos</v-tab>
+          <v-tab>Versões de Jogos</v-tab>
+          <v-tab>Evoluções</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="activeTab">
+          <v-tab-item>
+            <v-data-table
+              :headers="moveHeaders"
+              :items="filterMoves(selectedPokemon)"
+              item-key="move.name"
+              show-expand
+              class="elevation-1"
+            >
+              <template v-slot:expanded-item="{ headers, item }">
+                <td :colspan="headers.length">
+                  <v-list dense>
+                    <v-list-item v-for="version in item.version_group_details" :key="version.version_group.name">
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          {{ version.version_group.name }}: Level {{ version.level_learned_at }}
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </td>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+          <v-tab-item>
+            <v-data-table
+              :headers="gameHeaders"
+              :items="selectedPokemon.game_indices"
+              item-key="version.name"
+              class="elevation-1"
+            >
+              <template v-slot:item="{ item }">
+                <tr>
+                  <td>{{ item.version.name }}</td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+          <v-tab-item>
+            <v-row v-if="evolutions.length">
+              <v-col v-for="evolution in evolutions" :key="evolution.id" class="text-center">
+                <img :src="evolution.image" :alt="evolution.name" class="pokemon-image-evolutions" />
+                <div>{{ evolution.name }}</div>
+                <div v-if="evolution.level">Level: {{ evolution.level }}</div>
+              </v-col>
+            </v-row>
+            <v-row v-else>
+              <v-col class="text-center">
+                <div>Nenhuma evolução encontrada</div>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PokemonDialog',
+  props: {
+    showDialog: {
+      type: Boolean,
+      required: true,
     },
-    data() {
-      return {
-        activeTab: 0,
-        moveHeaders: [
-          { text: 'Level', value: 'level' },
-          { text: 'Nome', value: 'move.name' },
-        ],
-        gameHeaders: [
-          { text: 'Versão', value: 'version.name' },
-        ],
-      };
+    selectedPokemon: {
+      type: Object,
+      required: false,
     },
-    methods: {
-      getName(pokemon) {
-        return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-      },
-      getMoveLevel(move) {
-        for (let version of move.version_group_details) {
-          if (version.move_learn_method.name === 'level-up') {
-            return version.level_learned_at;
-          }
+  },
+  data() {
+    return {
+      activeTab: 0,
+      moveHeaders: [
+        { text: 'Level', value: 'level' },
+        { text: 'Nome', value: 'move.name' },
+      ],
+      gameHeaders: [
+        { text: 'Versão', value: 'version.name' },
+      ],
+      evolutions: [],
+    };
+  },
+  watch: {
+    selectedPokemon(newVal) {
+      if (newVal) {
+        this.fetchEvolutions(newVal);
+      }
+    },
+  },
+  methods: {
+    getName(pokemon) {
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    },
+
+    getMoveLevel(move) {
+      for (let version of move.version_group_details) {
+        if (version.move_learn_method.name === 'level-up') {
+          return version.level_learned_at;
         }
-        return 0;
-      },
-      filterMoves(pokemon) {
-        return pokemon.moves
-          .filter((item) => {
-            return item.version_group_details.some(
-              (version) => version.move_learn_method.name === 'level-up'
-            );
-          })
-          .map((item) => ({
-            ...item,
-            level: this.getMoveLevel(item),
-          }))
-          .sort((a, b) => a.level - b.level);
-      },
-      closeDialog() {
-        this.$emit('update:showDialog', false);
-      },
+      }
+      return 0;
     },
-  };
-  </script>
-  
-  <style scoped>
-  .pokemon-dialog {
-    background-color: #cad1e9;
-    color: rgb(0, 0, 0);
-    border-radius: 16px;
-    max-height: 80vh;
-    overflow-y: auto;
+    filterMoves(pokemon) {
+      return pokemon.moves
+        .filter((item) => {
+          return item.version_group_details.some(
+            (version) => version.move_learn_method.name === 'level-up'
+          );
+        })
+        .map((item) => ({
+          ...item,
+          level: this.getMoveLevel(item),
+        }))
+        .sort((a, b) => a.level - b.level);
+    },
+    async fetchEvolutions(pokemon) {
+      try {
+        const speciesResponse = await axios.get(pokemon.species.url);
+        const evolutionChainUrl = speciesResponse.data.evolution_chain.url;
+        const evolutionResponse = await axios.get(evolutionChainUrl);
+        this.evolutions = this.parseEvolutions(evolutionResponse.data.chain);
+      } catch (error) {
+        console.error('Erro ao buscar evoluções:', error);
+      }
+    },
+    parseEvolutions(chain) {
+      const evolutions = [];
+      let current = chain;
+
+      while (current) {
+        const level = current.evolution_details.length ? current.evolution_details[0].min_level : null;
+        evolutions.push({
+          id: this.getPokemonIdFromUrl(current.species.url),
+          name: current.species.name,
+          level: level,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.getPokemonIdFromUrl(current.species.url)}.png`,
+        });
+        current = current.evolves_to.length ? current.evolves_to[0] : null;
+      }
+
+      return evolutions;
+    },
+    getPokemonIdFromUrl(url) {
+      const parts = url.split('/');
+      return parts[parts.length - 2];
+    },
+    getTypeColor(type) {
+      switch (type) {
+        case "grass":
+          return "green";
+        case "poison":
+          return "purple";
+        case "water":
+          return "blue";
+        case "bug":
+          return "light-green";
+        case "ground":
+          return "lime darken-3";
+        case "fairy":
+          return "pink lighten-4";
+        case "fighting":
+          return "red darken-4";
+        case "fire":
+          return "red";
+        case "flying":
+          return "teal";
+        case "psychic":
+          return "pink";
+        case "electric":
+          return "yellow darken-2";
+        case "rock":
+          return "brown";
+        case "ice":
+          return "cyan";
+        case "dragon":
+          return "purple";
+        case "normal":
+          return "grey";
+        default:
+          return "grey";
+      }
+    },
+    closeDialog() {
+      this.$emit('update:showDialog', false);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-dialog {
+  background-color: #cad1e9;
+  color: rgb(0, 0, 0);
+  border-radius: 16px;
+  max-height: 80vh;
+  overflow-y: auto;
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.pokemon-image-front {
+  width: 80%;
+}
+
+.pokemon-image {
+  width: 50%;
+}
+
+.pokemon-image-evolutions {
+  width: 50%;
+}
+
+.v-chip {
+  font-size: 2vh;
+}
+
+@media (max-width: 600px) {
+  .pokemon-id {
+    font-size: 1.8vw;
   }
-  
-  .close-btn {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-  }
-  
   .pokemon-image-front {
-    width: 80%;
+    width: 130%;
   }
-  
   .pokemon-image {
-    width: 50%;
+    width: 100%;
   }
-  
+
+  .pokemon-image-evolutions {
+  width: 80%;
+}
   .v-chip {
-    font-size: 2vh;
+    font-size: 60%;
   }
-  
-  @media (max-width: 600px) {
-    .pokemon-id {
-      font-size: 1.8vw;
-    }
-    .pokemon-image-front {
-      width: 130%;
-    }
-    .pokemon-image {
-      width: 100%;
-    }
-    .v-chip {
-      font-size: 60%;
-    }
-  }
-  </style>
+}
+</style>
